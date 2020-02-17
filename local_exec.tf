474d8c930a9a6c0d82a71fe930c6ff963f363810
surajkumarsahoo <31088022+surajkumarsahoo@users.noreply.github.com>
2020-02-17 21:57:04 +0530

    Update local_exec.tf

--- local_exec.tf
+++ local_exec.tf
@@ -35,7 +35,7 @@ resource "null_resource" "remote-exec-1" {
 resource "null_resource" "ansible-main" {
 provisioner "local-exec" {
   command = <<EOT
-        sleep 100;
+        sleep 60;
         > jenkins-ci.ini;
         echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
         export ANSIBLE_HOST_KEY_CHECKING=False;
