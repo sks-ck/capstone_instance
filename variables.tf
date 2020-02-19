9123b0f749dc1bd6335f83686d9683de4e460f44
surajkumarsahoo <31088022+surajkumarsahoo@users.noreply.github.com>
2020-02-19 12:37:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,11 @@ variable "sg-id" {
 variable "ami_id" {
   default = "ami-0620d12a9cf777c87"
 }
+
+variable "vm_type" {
+  default = "t2.micro"
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
